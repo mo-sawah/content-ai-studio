562c4b8e790331c43217bf7f1ae3159072841520
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-27 15:57:28 +0300

    feat: Implement full creative article generator UI and logic - build

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element'), 'version' => 'a8a03f069c5b8335b82f');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element'), 'version' => '17abc28947959a9e4199');
