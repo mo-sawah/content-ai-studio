2d0b0137875cb806a967d24de47e406d046aeec6
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-29 02:53:12 +0300

    Bump version to 1.6.17; update plugin version constants and improve styles for better UI consistency

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'c8f313bc2fece83373d6');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element'), 'version' => '921ef755aeec7a4abab8');
