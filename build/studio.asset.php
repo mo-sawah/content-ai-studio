a4f6fe68fc99c1b712231d18975bf338a8efe537
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-29 01:36:09 +0300

    Refactor styles for improved UI; update container background, button styles, and form controls for better aesthetics and usability

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element'), 'version' => '2f11489980250ca1bc4f');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element'), 'version' => '37a342dc411b252594bf');
