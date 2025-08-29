843cda95c547eff361a9a84fbcb2c54d213d1196
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-30 01:36:09 +0300

    Update version and styles for article-to-media
    - Bump version from 1.6.59 to 1.6.60 in package.json
    - Update asset version in studio.asset.php
    - Modify studio.css to enhance button styles and layout for better responsiveness
    - Refactor studio.scss to apply styles specifically to autocomplete container buttons

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '75838e5515f58b30e7d2');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'cda32d27d1a1473c1868');
