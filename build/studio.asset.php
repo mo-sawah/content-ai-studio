f538f7d6b30db543fde6feb39d1cf2a81e5a6b8d
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-30 01:29:07 +0300

    Update version and styles for article-to-media
    - Bump version from 1.6.55 to 1.6.56 in package.json
    - Update asset version in studio.asset.php
    - Modify studio.css to adjust button styles and layout for better responsiveness
    - Add max-height to primary buttons in video search container in studio.scss

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '4dbec97046be6f9916cb');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '5e8dd0ac465cea8e1af9');
