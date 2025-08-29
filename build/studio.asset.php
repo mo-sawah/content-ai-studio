d35fe0148e5e3e8963a560aa986f3ed78ad738e7
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-29 13:39:29 +0300

    Bump version to 1.6.27; update plugin version constants; enhance dropdown styling and positioning for better UI consistency.

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'a2550e837a56c86cd8a6');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '2b7eb55d5df333cffe2c');
