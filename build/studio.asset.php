5ce77399e0c0fb559f90f07a97bc2515b101631f
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-30 01:31:33 +0300

    Update version and styles for article-to-media
    - Bump version from 1.6.56 to 1.6.57 in package.json
    - Update asset version in studio.asset.php
    - Modify studio.css to enhance button styles and layout
    - Add new styles for filter button in studio.scss

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '5e8dd0ac465cea8e1af9');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '75838e5515f58b30e7d2');
