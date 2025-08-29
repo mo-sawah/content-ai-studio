eac7fd0a56e5afc00824b7c0553504863866a056
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-30 01:59:44 +0300

    Update version and styles for article-to-media
    - Bump version from 1.6.61 to 1.6.62 in package.json
    - Update asset version in studio.asset.php
    - Modify studio.css to adjust button margins and improve layout
    - Add margin-bottom to buttons in studio.scss for better spacing

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'c340a44a4f0dad6c0c52');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '362ba01c796cd6340be2');
