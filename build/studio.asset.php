36bd0e9acab8305d19fca490c25c0d46459132f2
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-30 05:27:04 +0300

    Update asset version, enhance CSS styles, and modify SCSS for button alignment
    - Updated the version in studio.asset.php from '78e107252511783d4825' to '04add3a80647798e53a7'.
    - Refactored studio.css to improve button styles and layout, ensuring better responsiveness and visual consistency.
    - Added flex properties to studio.scss for buttons to align items and create a gap for better spacing.
    - Updated plugin-file-list.txt to reflect changes in binary files.

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '78e107252511783d4825');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '04add3a80647798e53a7');
