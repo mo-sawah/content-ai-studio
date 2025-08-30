4244a2e83098222bd95d34cb9ad1edee6b1326a7
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-30 08:34:16 +0300

    fix: change background color of chart preview container to white

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '96fd8e7ea108b8f2160d');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '461ccce9259d311677df');
