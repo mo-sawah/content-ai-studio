aa18447dbaec913c3f734a30bcd5a308326c7e5d
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-08-30 10:33:49 +0300

    Update styles and versioning for studio assets
    - Updated version in studio.asset.php to '3e9f2f4c79118caa7b75'.
    - Refactored studio.css to improve button styles and layout consistency, including adjustments for primary and secondary buttons.
    - Added new styles for secondary buttons in studio.scss to enhance visual consistency and interactivity.

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'c64954d219cf7ad04083');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '3e9f2f4c79118caa7b75');
