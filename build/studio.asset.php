965c3001e93cb2172f5f8b35dc4e1c7c229cfafa
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-05 01:42:54 +0300

    Enhance admin form styles with high specificity overrides for inputs, buttons, and labels

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '3d483afbafee792dacbb');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '3ed6ae9abccceb66251d');
