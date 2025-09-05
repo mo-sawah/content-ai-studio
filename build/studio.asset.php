26b765f71f70e8238cca6a23e7aa13ddd0596312
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-05 03:50:27 +0300

    Add yellow/amber gradient definitions and apply to action icons
    - Updated margin for the title to improve spacing.
    - Introduced new gradient classes for yellow and amber colors.
    - Added fallback gradient for action icons using the new yellow/amber classes.

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '8ae2c5f2e0c5948ac577');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'ba4eeafd7850f174229b');
