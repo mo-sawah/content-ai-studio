170d022233b447271fb7c8e8bf3147eeb97be428
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-07 16:43:21 +0300

    Refactor button margins in admin studio styles
    - Updated margin for .components-button and button elements to 0
    - Removed unnecessary margin from buttons to improve layout consistency

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '8f9f485a651de36ed6dd');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '924f438d4743c45800dc');
