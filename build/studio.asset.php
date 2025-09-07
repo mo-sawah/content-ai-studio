8b72c8c79f6abc82110b4754d8165120c3ea4833
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-08 01:44:55 +0300

    Adjust grid layout for atm-type-cards: reduce min-width to 250px and set max-width to 100%

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '2b97816863d7e0fef750');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '3f8df72b94e98219f004');
