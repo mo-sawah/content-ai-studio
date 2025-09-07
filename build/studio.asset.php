99df02ceddd79baccb9adcb84403aff9b73c7319
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-07 16:45:14 +0300

    Fix SCSS syntax error by closing the body.wp-admin .atm-content-body rule

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '924f438d4743c45800dc');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'b8b12f0cd7594b31e3b1');
