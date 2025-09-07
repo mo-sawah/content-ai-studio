ed1fba34f7af01d1f28f335881a0bea52d8a5d1e
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-07 17:53:56 +0300

    style: add margin to buttons in the form container for better spacing

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '19f4d9be14c4b6c7b010');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '6ee429249d9936dc205c');
