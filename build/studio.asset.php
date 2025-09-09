3c82861cef37fa700698e6ae733fdd688dee26ec
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-09 09:40:34 +0300

    fix: update border-radius for podcast script textarea in admin styles

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'fb2e9c618164566ce198');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '2b8bbd5f16a032b470ba');
