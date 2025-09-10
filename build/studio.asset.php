f454635bc25dd2d009f8452d0fb9e870eacf9b33
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-11 02:48:25 +0300

    Refactor button styles for disabled states in studio.scss
    - Introduced a new class structure for ATM buttons with specific styles for disabled states.
    - Updated styles for .atm-btn, .atm-btn-primary, .atm-btn-secondary, and .atm-btn-outline to ensure consistent appearance when disabled.
    - Removed spinners from disabled buttons and ensured pointer events are disabled.
    - Enhanced hover states for disabled buttons to maintain visual consistency.

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '8efdd0f9afe5872cd4d8');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'bfd5ab68f5519e88a454');
