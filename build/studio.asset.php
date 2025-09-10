f452b1273dbbc7e7f2cdc0d047929d803d112781
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-10 11:43:56 +0300

    Add Tailwind-like utility variables for violet-500 and update gradient utility class
    - Introduced utility variables for violet-500 to facilitate gradient usage.
    - Updated the gradient utility class for atm-action-icon to ensure consistent styling with !important flag.

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '1723d499063f4b17736c');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '7f89ace24aecbc09da6c');
