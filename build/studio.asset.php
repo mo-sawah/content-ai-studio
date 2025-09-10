373938ef3799cb768ddef0078e28ad06525126c8
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-11 02:42:46 +0300

    Enhance button styles for disabled states
    - Updated styles for disabled buttons in the studio container to improve visibility and user experience.
    - Removed spinners from disabled buttons to prevent confusion.
    - Added specific styles for primary and secondary disabled buttons to differentiate them clearly.
    - Applied consistent styling for humanize component buttons when disabled.

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '7f89ace24aecbc09da6c');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '8efdd0f9afe5872cd4d8');
