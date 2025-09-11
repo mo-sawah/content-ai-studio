c02dd5332cb4abbd63d2e3b709b4b119ac5fa832
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-11 03:44:10 +0300

    fix: hide disabled button after pseudo-element

--- build/studio.asset.php
+++ build/studio.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'da4d5fcacf396627dcd3');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '2a1f09de6e6c33c0e839');
