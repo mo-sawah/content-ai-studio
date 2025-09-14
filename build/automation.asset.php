54778ee235f9eceb39485bb31e28fa3c01e6c674
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-14 09:15:26 +0300

    Implement code changes to enhance functionality and improve performance

--- build/automation.asset.php
+++ build/automation.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '5fa0a19a688a9ab4b41e');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => 'b0ee119a89efa69384e4');
