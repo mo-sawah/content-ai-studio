9b88c5c2d8f2b5c66ce5efe2ac5837f45261de97
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-14 13:17:05 +0300

    Enhance button styling and layout in automation.scss
    - Added margin to buttons within the ATM automation root for better spacing.
    - Set secondary buttons in the publishing grid to full width for improved usability.
    - Increased bottom margin for description paragraphs in form sections.
    - Adjusted dropdown button styles for better alignment and spacing of text and icons.

--- build/automation.asset.php
+++ build/automation.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => 'dc7e13e758d026838b28');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '0c4365392af8312b4565');
