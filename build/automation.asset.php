3e978eff7e3c5263a2bbfbe2928553a3554f59d4
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-14 14:00:06 +0300

    Update background color in automation.scss for active and hover states
    - Changed background color from #faf5ff to #f5f7ff for improved visual consistency
    - Updated styles for active, hover, and focus states across multiple components

--- build/automation.asset.php
+++ build/automation.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => 'b5c6fd42f7fd1745e1f0');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '0ddbed233951d05e5d55');
